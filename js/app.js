647aa82c6a4432f4f25cdaa000476543155cda41
l-kosmala <l.kosmala.1985@gmail.com>
2022-03-06 14:49:13 +0100

    Update app.js
    Basic js structure.

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// local reviews data
+const reviews = [
+    {
+        id: 1,
+        userName: "susan smith",
+        job: "web developer",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+        textReview: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+
+    },
+    {
+        id: 2,
+        userName: "anna johnson",
+        job: "intern",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        textReview: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    }
+    {
+        id: 3,
+        userName: "bill anderson",
+        job: "the boss",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        textReview: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+];
