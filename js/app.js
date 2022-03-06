906da8852f965ba70e41672a683ce50eea51a70a
l-kosmala <l.kosmala.1985@gmail.com>
2022-03-06 16:09:44 +0100

    Index and app files
    Updated index.html and create scripts to app.js

--- js/app.js
+++ js/app.js
@@ -11,15 +11,73 @@ const reviews = [
     {
         id: 2,
         userName: "anna johnson",
-        job: "intern",
+        job: "web designer",
         img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
         textReview: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    }
+    },
     {
         id: 3,
+        userName: "peter jones",
+        job: "intern",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+        textReview: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+        id: 4,
         userName: "bill anderson",
         job: "the boss",
         img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
         textReview: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
     },
 ];
+
+// select items 
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const btnPrev = document.querySelector('.prev-btn');
+const btnNext = document.querySelector('.next-btn');
+const btnRandom = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+    showPerson(currentItem);
+});
+
+// show person base on item
+function showPerson(person) {
+    const item = reviews[person];
+    author.textContent = item.userName;
+    job.textContent = item.job;
+    img.src = item.img;
+    info.textContent = item.textReview;
+};
+
+// show next person
+btnNext.addEventListener('click', function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+// show prev person
+btnPrev.addEventListener('click', function () {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+// random person button
+btnRandom.addEventListener('click', function () {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+});
